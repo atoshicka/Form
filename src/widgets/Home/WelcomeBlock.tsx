28960247dc6d93c26db976e2607e3de8d72e70c6
atoshicka <xiaokisamurmur@mail.ru>
2026-05-16 18:12:19 +0500

    log out button

--- src/widgets/Home/WelcomeBlock.tsx
+++ src/widgets/Home/WelcomeBlock.tsx
@@ -1,9 +1,18 @@
-import styles from "./WelcomeBlock.module.css";
+import { useNavigate } from "react-router-dom";
+import './WelcomeBlock.css';
 
 export const WelcomeBlock = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.hello}>Hello</h1>
+    <div className="wrapper">
+      <h1 className="hello">Hello</h1>
+      <button onClick={handleLogout} className="logoutBtn">log out</button>
     </div>
   );
 };
